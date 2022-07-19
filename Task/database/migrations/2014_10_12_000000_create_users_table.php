68817e63b9e564b04897513e3c5f030fe0b69c09
Doston1316 <84778369+Doston1316@users.noreply.github.com>
2022-07-19 13:03:00 +0500

    1

--- Task/database/migrations/2014_10_12_000000_create_users_table.php
+++ Task/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,10 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->bigInteger('role_id')->unsigned();
             $table->rememberToken();
             $table->timestamps();
+
         });
     }
 
