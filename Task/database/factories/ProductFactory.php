fde125b4f61da54de75e13221d2c0573a79719e9
Doston1316 <84778369+Doston1316@users.noreply.github.com>
2022-07-25 10:06:03 +0500

    1

---
+++ Task/database/factories/ProductFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title'=>$this->faker->text(15),
+            'img'=>$this->faker->text(6).'jpg',
+            'category_id'=>$this->faker->numberBetween(1,10)
+        ];
+    }
+}
