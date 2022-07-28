0d353d86158016a1046a5d63023d2f4eb16151be
Doston1316 <84778369+Doston1316@users.noreply.github.com>
2022-07-28 14:59:21 +0500

    First Commit

--- Internet-magazin/app/Models/Product.php
+++ Internet-magazin/app/Models/Product.php
@@ -17,4 +17,14 @@ class Product extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function cart()
+    {
+        return $this->belongsTo(Cart::class);
+    }
+
+    public function orderItem()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
